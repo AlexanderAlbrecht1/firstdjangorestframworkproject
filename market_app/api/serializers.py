306e13f34950b0f4be8a85d2413dc487ceead1d5
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-26 13:45:31 +0200

    custom validation

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 from market_app.models import Market
 
+def validate_no_x(value):
+        errors = []
+
+        if "X" or "x" in value:
+            errors.append('Bitte kein Schweinskram')
+        if "Y" in value:
+            pass
+            errors.append('Bitte kein Schweinskram mit Y')
+
+        if errors:
+             raise serializers.ValidationError(errors)
+        return value
+
 class MarketSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=225)
+    location = serializers.CharField(validators=[validate_no_x],max_length=225)
     description = serializers.CharField()
     net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
 
@@ -18,3 +31,4 @@ class MarketSerializer(serializers.Serializer):
         instance.net_worth = validated_data.get('net_worth', instance.net_worth)
         instance.save()
         return instance
+    
