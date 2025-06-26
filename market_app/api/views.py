a061e17c5f6e29170e535edd7c8eead4a251ba21
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-26 13:08:13 +0200

    Serializer DELETE and PUT

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -19,5 +19,27 @@ def markets_view(request):
             return Response(serializer.data)
         else: 
             return Response(serializer.errors)
-        
+
+@api_view(['GET', "DELETE", "PUT"])
+def market_single_view(request, pk):
+
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)  
+      
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data) 
+    
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors)     
             
