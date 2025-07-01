e1b679027427bc0c896495e45b43283fb810691c
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-07-01 11:14:21 +0200

    Product serializer

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from market_app.models import Market, Seller
+from market_app.models import Market, Seller, Product
 
 def validate_no_x(value):
         errors = []
@@ -57,3 +57,41 @@ class SellerCreateSerializer(serializers.Serializer):
          markets = Market.objects.filter(id__in = market_ids)
          seller.markets.set(markets)
          return seller
+     
+class ProductDetailSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    price = serializers.DecimalField(max_digits=50, decimal_places=2)
+    market = serializers.StringRelatedField(many=True)
+    seller = serializers.StringRelatedField(many=True)
+
+
+class ProductCreateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    price = serializers.DecimalField(max_digits=50, decimal_places=2)
+    market = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    seller = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+
+    def validate_markets(self, value):
+        market = Market.objects.filter(id__in = value)
+        if len(market) != len(value):
+            raise serializers.ValidationError("market id not found")
+        return value
+    
+    def validate_seller(self, value):
+        seller = Seller.objects.filter(id__in = value)
+        if len(seller) != len(value):
+            raise serializers.ValidationError("market id not found")
+        return value
+
+    def create(self, validated_data):
+         market_ids = validated_data.pop('market')
+         seller_ids = validated_data.pop('seller')
+         market = Market.objects.filter(id__in = market_ids)
+         seller = Seller.objects.filter(id__in = seller_ids)
+         product = Product.objects.create(**validated_data)
+         product.market.set(market)
+         product.seller.set(seller)
+         return product
