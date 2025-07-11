82023f3f6db805fb74fb6d22f3e59e27dc200e5e
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-07-11 10:44:55 +0200

    model serializer

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from market_app.models import Market, Seller, Product
 
-def validate_no_x(value):
+
+
+class MarketSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Market
+        fields = '__all__'
+
+    def validate_name(self, value):
         errors = []
 
         if "X" in value:
@@ -14,23 +21,7 @@ def validate_no_x(value):
              raise serializers.ValidationError(errors)
         return value
 
-class MarketSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    location = serializers.CharField(validators=[validate_no_x],max_length=225)
-    description = serializers.CharField()
-    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
 
-    def create(self, validated_data):
-        return Market.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.location = validated_data.get('location', instance.location)
-        instance.description = validated_data.get('description', instance.description)
-        instance.net_worth = validated_data.get('net_worth', instance.net_worth)
-        instance.save()
-        return instance
     
 
 class SellerDetailSerializer(serializers.Serializer):
