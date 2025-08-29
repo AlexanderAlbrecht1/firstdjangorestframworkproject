819013378e1949ee0001320c461f7906e01ccb1d
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-08-29 22:09:17 +0200

    first permission

--- market_app/models.py
+++ market_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Market(models.Model):
     name = models.CharField(max_length=255)
@@ -26,3 +27,21 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.price})"
+    
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    net_worth = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class ManufacturerUser(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, blank=True, null=True)
+    joined_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.manufacturer.name})"
--- markt/settings.py
+++ markt/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'market_app'
+    'market_app',
+    'user_auth_app'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
